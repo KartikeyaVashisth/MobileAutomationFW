22b7f4b60737f98e4565b010c3b8dd9f6b930bc1
ideshmukh <indrajit.deshmukh@quicken.com>
2022-11-02 14:09:09 +0530

    favorite payee test suites automated

---
+++ src/test/java/dugout/FavoritePayeePage.java
@@ -0,0 +1,475 @@
+package dugout;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.support.PageFactory;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import referee.Commentary;
+import referee.Verify;
+import support.Engine;
+import support.Helper;
+import support.TransactionRecord;
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofMillis;
+
+public class FavoritePayeePage {
+	
+	public FavoritePayeePage () {
+		try {
+			PageFactory.initElements(new AppiumFieldDecorator(Engine.getDriver()), this);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name='Display Favorite Payees'`]")
+	@AndroidFindBy(xpath="//android.widget.TextView[@text='Display Favorite Payees']")
+    public MobileElement DisplayFavoritePayee;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeSwitch[3]")
+	@AndroidFindBy(xpath="//android.widget.Switch[3]")
+    public MobileElement FavoritePayeeSwitch;
+	
+
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeOther[`name='navigationMenu'`]")
+	@AndroidFindBy(xpath="//android.view.ViewGroup[@content-desc='navigationMenu']")
+    public MobileElement hambergerIcon;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name = 'Settings Menu'`]")
+	@AndroidFindBy(xpath="//android.widget.TextView[@text='Settings']")
+	public MobileElement settingsOption;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeOther[`name='favoriteHeaderMoreAction'`]")
+	@AndroidFindBy(xpath="//android.widget.ImageView[@content-desc='favoriteHeaderMoreAction']")
+    public MobileElement MoreOptionInPayeeDrawer;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeOther[`name='moreAction'`]")
+	@AndroidFindBy(xpath="//android.widget.ImageView[@content-desc='moreAction']")
+    public MobileElement YelpInPayeeDrawer;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeOther[`label ='disableYelpPayeeView'`][3]")
+	@AndroidFindBy(xpath="//android.widget.TextView[@text='Turn off Yelp recommendations']")
+    public MobileElement TurnOffYelp;
+
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name='Favorite Payee'`]")
+	@AndroidFindBy(xpath="//android.widget.TextView[@text='Favorite Payee']")
+    public MobileElement FavoritePayeeLabel;
+
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeOther[`label ='hideFavoritePayeeView'`][3]")
+    @AndroidFindBy(xpath="//android.widget.TextView[@text='Hide favorites']")
+    public MobileElement HideFavorite;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name='Favorite Payee'`]")
+	@AndroidFindBy(xpath="//android.widget.TextView[@text='Favorite Payee']")
+    public MobileElement btnEnableFavoritePayee;
+
+	//@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementStaticText[`name='Payee'`]")
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`label = 'Payee'`]")
+	@AndroidFindBy(xpath="//android.widget.TextView[@text='Payee']")
+    public MobileElement payee;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeOther[`name='All Payees'`][1]")
+	@AndroidFindBy(xpath = "//android.widget.TextView[@text='All Payees']")
+	public MobileElement allPayeesLabel;
+
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name='Near by'`][1]")
+	@AndroidFindBy(xpath = "//android.widget.TextView[@text='Near by']")
+	public MobileElement yelpNearByLabel;
+	
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeStaticText[`name CONTAINS 'Turn off Favorite Payee recommendations?'`]")
+	@AndroidFindBy(xpath="//android.widget.TextView[contains(@text,'Turn off Favorite Payee recommendations?')]")
+	public MobileElement HideFavoritePayeeConfMessage;
+
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeButton[`name='Yes'`]")
+	@AndroidFindBy(xpath="//*[@text='YES']")
+	public MobileElement buttonYES;
+	
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeButton[`name='No'`]")
+	@AndroidFindBy(xpath="//*[@text='NO']")
+	public MobileElement buttonNO;
+	
+	@iOSXCUITFindBy(iOSClassChain="**/XCUIElementTypeButton[`name contains 'back'`]")
+	@AndroidFindBy(xpath="//android.view.ViewGroup[@content-desc='backArrow']")
+	public MobileElement backButton;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeButton[`name ='Remove'`]")
+	@AndroidFindBy(xpath="//android.widget.Button[@text='REMOVE']")
+	public MobileElement removeBtn;
+	
+	@iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeButton[`name ='Cancel'`]")
+	@AndroidFindBy(xpath="//android.widget.Button[@text='CANCEL']")
+	public MobileElement cancelBtn;
+
+
+	
+    public void EnablefavoritPayee() throws Exception{
+		
+		hambergerIcon.click();
+		Thread.sleep(2000);
+		this.scrollDownToSettings();
+		Helper h = new Helper();
+		
+		settingsOption.click();
+		Thread.sleep(1000);
+		if (h.getEngine().equals("android")){
+		    if(FavoritePayeeSwitch.getAttribute("checked").equals("false")) {
+			   FavoritePayeeSwitch.click();
+		    }
+		}
+		else {
+			if(FavoritePayeeSwitch.getAttribute("value").equals("0")) {
+				FavoritePayeeSwitch.click();
+		    }
+		}
+		
+		
+		this.backButton.click();	
+		Thread.sleep(1000);
+	 }
+
+    public void DisablefavoritPayee() throws Exception{
+		
+		hambergerIcon.click();
+		Thread.sleep(2000);
+		this.scrollDownToSettings();
+		Helper h = new Helper();
+		
+		settingsOption.click();
+		Thread.sleep(1000);
+		if (h.getEngine().equals("android")){
+		    if(FavoritePayeeSwitch.getAttribute("checked").equals("true")) {
+			   FavoritePayeeSwitch.click();
+		    }
+		}
+		else {
+			if(FavoritePayeeSwitch.getAttribute("value").equals("1")) {
+				FavoritePayeeSwitch.click();
+		    }
+		}
+		this.backButton.click();
+		
+		Thread.sleep(1000);
+	 }
+
+    public void navigateToSettingMemu() throws Exception{
+		
+ 		hambergerIcon.click();
+ 		Thread.sleep(2000);
+ 		this.scrollDownToSettings();
+ 		
+ 		settingsOption.click();
+ 		Thread.sleep(1000);
+
+ 	  }
+ 	
+
+
+    public void scrollDownToSettings () throws Exception{
+	
+	    Helper h = new Helper();
+	
+	    if (h.getEngine().equals("android")) {
+		    Engine.getDriver().findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Settings\").instance(0))"));
+		    Thread.sleep(1000);
+	     }
+	
+	    else {
+		
+		     HashMap<String, String> scrollObject = new HashMap<String, String>();
+		     scrollObject.put("direction", "down");
+		     scrollObject.put("toVisible", "not an empty string");
+		     Engine.getDriver().executeScript("mobile:scroll", scrollObject);
+		     Thread.sleep(1000);
+		
+	         }
+        }
+
+    public void HideFavoritePayeeFromPayeeDrawer() throws Exception{
+    	
+    	
+    	TransactionDetailPage td = new TransactionDetailPage();
+        OverviewPage op = new  OverviewPage();
+    	
+    	op.addTransaction.click();
+    	
+        td.enterAmount("10.00");
+    	Thread.sleep(3000);
+    	//Engine.getDriver().hideKeyboard();
+    	this.payee.click();
+    	this.MoreOptionInPayeeDrawer.click();
+    	this.HideFavorite.click();
+    	Thread.sleep(1000);
+    	this.buttonYES.click();
+    	Thread.sleep(3000);
+ 
+    	
+    }
+    
+  public void AddPayeeAsFavPayee(String payee) throws Exception{
+ 		
+ 		Helper h = new Helper();
+ 		
+ 		if (h.getEngine().equals("ios"))
+ 		  AddPayeeAsFavPayee_ios(payee);
+ 		else
+ 		  AddPayeeAsFavPayee_android(payee);
+ 		
+ 	}
+    
+   public void AddPayeeAsFavPayee_ios(String payee) throws Exception{
+    	
+    	MobileElement ele_ios = Engine.getDriver().findElementByXPath("//XCUIElementTypeStaticText[@name='"+payee+"']/..");
+	
+				JavascriptExecutor js = (JavascriptExecutor)Engine.getDriver();
+				HashMap scrollObject = new HashMap();
+				scrollObject.put("direction", "left"); 
+				scrollObject.put("element", ele_ios); 
+				js.executeScript("mobile: swipe", scrollObject);
+				Thread.sleep(500);
+				
+		MobileElement fav_icon = Engine.getDriver().findElementByXPath("(//XCUIElementTypeStaticText[@name='"+payee+"']/../../../XCUIElementTypeOther/XCUIElementTypeOther)[2]");
+			
+			fav_icon.click();
+			
+		}
+ 
+   public void AddPayeeAsFavPayee_android(String payee) throws Exception{
+	    
+   	
+   
+       MobileElement startElement = Engine.getDriver().findElementByXPath("//android.widget.TextView[@text='"+payee+"']/../..");
+		
+	   int startX = startElement.getLocation().getX() + (startElement.getSize().getWidth() / 2);
+       int startY = startElement.getLocation().getY() + (startElement.getSize().getHeight() / 2);
+       int endX = startElement.getLocation().getX() + (startElement.getSize().getWidth() / 3);
+       int endY = startElement.getLocation().getY() + (startElement.getSize().getHeight() / 2);
+       new TouchAction(Engine.getDriver())
+           .press(point(startX,startY))
+           .waitAction(waitOptions(ofMillis(1000)))
+           .moveTo(point(endX, endY))
+           .release().perform();
+   	
+ 
+		MobileElement fav_icon = Engine.getDriver().findElementByXPath("//android.widget.TextView[@text='"+payee+"']/../../..//android.widget.ImageView");
+			
+			fav_icon.click();
+		  	//Thread.sleep(3000);
+			
+		}
+   
+   
+   public void RemoveFavPayee(String payee) throws Exception{
+		
+		Helper h = new Helper();
+		
+		if (h.getEngine().equals("ios"))
+			RemoveFavPayee_ios(payee);
+		else
+			RemoveFavPayee_android(payee);
+		
+	}
+
+    
+   
+    public void RemoveFavPayee_ios(String payee) throws Exception{
+    	
+    	
+    	MobileElement ele_ios = Engine.getDriver().findElementByXPath("//XCUIElementTypeStaticText[@name='"+payee+"']/..");
+	
+				JavascriptExecutor js = (JavascriptExecutor)Engine.getDriver();
+				HashMap scrollObject = new HashMap();
+				scrollObject.put("direction", "left"); 
+				scrollObject.put("element", ele_ios); 
+				js.executeScript("mobile: swipe", scrollObject);
+				Thread.sleep(500);
+				
+		MobileElement fav_icon = Engine.getDriver().findElementByXPath("(//XCUIElementTypeStaticText[@name='"+payee+"']/../../../XCUIElementTypeOther/XCUIElementTypeOther)[2]");
+			
+			fav_icon.click();
+			Thread.sleep(500);
+			this.removeBtn.click();
+			Thread.sleep(3000);
+			
+		}
+    
+ 
+ 
+   public void RemoveFavPayee_android(String payee) throws Exception{
+ 	
+ 
+        MobileElement startElement = Engine.getDriver().findElementByXPath("//android.widget.TextView[@text='"+payee+"']/../..");
+		
+		int startX = startElement.getLocation().getX() + (startElement.getSize().getWidth() / 2);
+        int startY = startElement.getLocation().getY() + (startElement.getSize().getHeight() / 2);
+        int endX = startElement.getLocation().getX() + (startElement.getSize().getWidth() / 3);
+        int endY = startElement.getLocation().getY() + (startElement.getSize().getHeight() / 2);
+        new TouchAction(Engine.getDriver())
+         .press(point(startX,startY))
+         .waitAction(waitOptions(ofMillis(1000)))
+         .moveTo(point(endX, endY))
+         .release().perform();
+ 	
+
+		MobileElement fav_icon = Engine.getDriver().findElementByXPath("//android.widget.TextView[@text='"+payee+"']/../../..//android.widget.ImageView");
+			
+			fav_icon.click();
+			Thread.sleep(2000);
+			this.removeBtn.click();
+			Thread.sleep(3000);
+						
+		}
+   
+   public void verifyFavPayee(String payee) throws Exception{
+		
+		Helper h = new Helper();
+		
+		if (h.getEngine().equals("ios"))
+			verifyFavPayee_ios(payee);
+		else
+			verifyFavPayee_android(payee);
+		
+	}
+
+   
+   public void verifyFavPayee_ios(String payee) throws Exception{
+	   
+	   TransactionDetailPage td = new TransactionDetailPage();
+	   
+	    this.payee.click();
+	    td.searchPayee.sendKeys(payee);
+		Helper h = new Helper();
+		h.hideKeyBoard();
+		Thread.sleep(3000);
+	   
+	   if(Verify.objExists(FavoritePayeeLabel)) {
+			String cc = "//XCUIElementTypeStaticText[@name='"+payee+"']/..";
+			Engine.getDriver().findElementByXPath(cc).isDisplayed();
+			
+			Thread.sleep(500);
+			Commentary.log(LogStatus.FAIL,"FAIL: Showing Payee "+payee+" as Favorite Payee ");
+		}
+		else {
+
+			Commentary.log(LogStatus.PASS,"PASS: Not showing Payee "+payee+" as Favorite Payee. ");
+		}
+	   
+   }
+	   
+	   
+	public void verifyFavPayee_android(String payee) throws Exception{
+		
+		 TransactionDetailPage td = new TransactionDetailPage();
+		   
+		    td.searchPayee.click();
+		    td.searchPayee.sendKeys(payee);
+			Helper h = new Helper();
+			h.hideKeyBoard();
+			Thread.sleep(3000);
+		   
+		   
+		   if(Verify.objExists(FavoritePayeeLabel)) {
+				String cc = "//android.widget.TextView[@text='"+payee+"']/../..";
+				Engine.getDriver().findElementByXPath(cc).isDisplayed();
+				
+				Thread.sleep(500);
+				Commentary.log(LogStatus.PASS,"PASS: Showing Payee "+payee+" as Favorite Payee ");
+			}
+			else {
+
+				Commentary.log(LogStatus.FAIL,"FAIL: Not showing Payee "+payee+" as Favorite Payee. ");
+			}
+		   
+	   }
+	
+	
+   public void verifyRemoveFavPayee(String payee) throws Exception{
+				
+				Helper h = new Helper();
+				
+				if (h.getEngine().equals("ios"))
+					verifyFavPayee_ios(payee);
+				else
+					verifyFavPayee_android(payee);
+				
+			}
+
+		   
+		   public void verifyRemoveFavPayee_ios(String payee) throws Exception{
+			   
+			   TransactionDetailPage td = new TransactionDetailPage();
+			   
+			    this.payee.click();
+			    td.searchPayee.sendKeys(payee);
+				Helper h = new Helper();
+				h.hideKeyBoard();
+				Thread.sleep(3000);
+			   
+			   if(Verify.objExists(FavoritePayeeLabel)) {
+					String cc = "//XCUIElementTypeStaticText[@name='"+payee+"']/..";
+					Engine.getDriver().findElementByXPath(cc).isDisplayed();
+					
+					Thread.sleep(500);
+					Commentary.log(LogStatus.FAIL,"FAIL: Unable to remove Payee "+payee+" as Favorite Payee ");
+				}
+				else {
+
+					Commentary.log(LogStatus.PASS,"PASS: Able to remove "+payee+" as Favorite Payee. ");
+				}
+			   
+		   }
+			   
+			   
+			public void verifyRemoveFavPayee_android(String payee) throws Exception{
+				
+				 TransactionDetailPage td = new TransactionDetailPage();
+				   
+				    td.searchPayee.click();
+				    td.searchPayee.sendKeys(payee);
+					Helper h = new Helper();
+					h.hideKeyBoard();
+					Thread.sleep(3000);
+				   
+				   
+				   if(Verify.objExists(FavoritePayeeLabel)) {
+						String cc = "//android.widget.TextView[@text='"+payee+"']/../..";
+						Engine.getDriver().findElementByXPath(cc).isDisplayed();
+						
+						Thread.sleep(500);
+						Commentary.log(LogStatus.FAIL,"FAIL: Unable to remove Payee "+payee+" as Favorite Payee ");
+					}
+					else {
+
+						Commentary.log(LogStatus.PASS,"PASS: Able to remove "+payee+" as Favorite Payee. ");
+					}
+				   
+				      	   
+		      
+	   
+   }
+ 
+ 
+ 
+ 
+ 
+}
---
+++ src/test/java/tournament/FavoritePayee_Test.java
@@ -0,0 +1,360 @@
+package tournament;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import dugout.BankingAndCreditCardPage;
+import dugout.BillsAndIncomePage;
+import dugout.FavoritePayeePage;
+import dugout.OverviewPage;
+import dugout.SignInPage;
+import dugout.TagManagementPage;
+import dugout.TransactionDetailPage;
+import dugout.TransactionsPage;
+import referee.Commentary;
+import referee.Verify;
+import support.Engine;
+import support.Helper;
+import support.Recovery;
+import support.TransactionRecord;
+
+public class FavoritePayee_Test extends Recovery {
+	String sUsername = "quicken789@gmail.com";
+	String sPassword = "Quicken@01";
+	String sDataset = "Search_functionality_test";
+	
+	@Test (priority = 1, enabled = true)
+	public void FP1_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+		
+		SignInPage si = new SignInPage();
+		//si.signIn(sUsername, sPassword, sDataset);
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the Display Favorite Payees option is present under the Settings section of the Hamburger menu.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+		fp.hambergerIcon.click();
+		Thread.sleep(3000);
+		fp.scrollDownToSettings();
+
+	    fp.settingsOption.click();
+		Thread.sleep(2000);
+		
+		
+		if(Verify.objExists(fp.DisplayFavoritePayee))
+			Commentary.log(sa,LogStatus.PASS, "Pass:Display Favorite Payees option is present under the Settings section of the Hamburger menu.");
+		else
+			Commentary.log(sa, LogStatus.FAIL, "FAIL: Display Favorite Payees option is not present under the Settings section of the Hamburger menu.");
+
+		sa.assertAll();
+		
+	}
+	
+	
+	@Test (priority = 2, enabled = true)
+	public void FP2_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+		
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the Favorite Payees section is seen on the Payee drawer when adding payees on the Add transactions page with 'Display Favorite Payees' is enabled.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+    	TransactionDetailPage td = new TransactionDetailPage();
+    	
+
+    	fp.EnablefavoritPayee();
+    	op.addTransaction.click();
+    	td.enterAmount("10.00");
+      	Thread.sleep(3000);
+    	fp.payee.click();
+    
+    	if(Verify.objExists(fp.FavoritePayeeLabel))
+    		
+    		Commentary.log(sa,LogStatus.PASS, "Pass:Favorite Payees section is present in the Payee drawer");
+		else
+			Commentary.log(sa, LogStatus.FAIL, "FAIL: Favorite Payees section is not present in the Payee drawer");
+    
+    	Thread.sleep(5000);
+
+		sa.assertAll();
+    	
+    }
+		
+	@Test (priority = 3, enabled = true)
+	public void FP3_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+		
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the Favorite Payees section is seen on the Payee drawer when editing payees on the View transactions page with 'Display Favorite Payees' is enabled.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		BankingAndCreditCardPage bcc = new BankingAndCreditCardPage();
+        TransactionsPage tp = new TransactionsPage();
+    	
+    	Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+        op.navigateToAcctList();
+    	bcc.allTransactionButton.click();
+    	tp.tapOnFirstTransaction();
+        fp.payee.click();
+    
+    	if(Verify.objExists(fp.FavoritePayeeLabel))
+    		
+    		Commentary.log(sa,LogStatus.PASS, "Pass:Favorite Payees section is present in the Payee drawer when editing payees on the View transactions page.");
+		else
+			Commentary.log(sa, LogStatus.FAIL, "FAIL: Favorite Payees section is not present in the Payee drawer when editing payees on the View transactions page.");
+    
+    	Thread.sleep(5000);
+
+		sa.assertAll();
+    	
+    }
+	
+	
+	@Test (priority = 4, enabled = true)
+	public void FP4_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+		
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify the sequence order of the sections when both 'Display Yelp Recommendation' and 'Display Favorite Payees' options are enabled..");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+        TransactionDetailPage td = new TransactionDetailPage();
+    	
+    	Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+    	op.addTransaction.click();
+    	td.enterAmount("10.00");
+      	Thread.sleep(3000);
+    	fp.payee.click();
+    	
+        if(Verify.objExists(fp.yelpNearByLabel))
+    		
+    		Commentary.log(sa,LogStatus.PASS, "Pass: Display Yelp Recommendation section is present in the Payee drawer on the View transactions page.");
+		else
+			Commentary.log(sa, LogStatus.FAIL, "FAIL: Display Yelp Recommendation section is not present in the Payee drawer when on the View transactions page.");
+    
+    
+    	if(Verify.objExists(fp.FavoritePayeeLabel))
+    		
+    		Commentary.log(sa,LogStatus.PASS, "Pass: Favorite Payees section is present in the Payee drawer on the View transactions page.");
+		else
+			Commentary.log(sa, LogStatus.FAIL, "FAIL: Favorite Payees section is not present in the Payee drawer on the View transactions page.");
+    
+        if(Verify.objExists(fp.allPayeesLabel))
+    		
+    		Commentary.log(sa,LogStatus.PASS, "Pass: All Payees section is present in the Payee drawer on the View transactions page.");
+		else
+			Commentary.log(sa, LogStatus.FAIL, "FAIL: All Payees section is not present in the Payee drawer on the View transactions page.");
+     
+    	
+    	Thread.sleep(5000);
+
+		sa.assertAll();
+    	
+    }
+	
+	
+	@Test (priority = 5, enabled = true)
+	public void FP5_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the Favorite Payees section should NOT be seen on the Payee drawer when adding payees on the Add transactions page with 'Display Favorite Payees' disabled.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		TransactionDetailPage td = new TransactionDetailPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+		fp.DisablefavoritPayee();
+
+		op.addTransaction.click();
+    	td.enterAmount("10.00");
+      	Thread.sleep(3000);
+    	fp.payee.click();
+    
+    	if(Verify.objExists(fp.FavoritePayeeLabel))
+    		
+    		Commentary.log(sa,LogStatus.FAIL, "FAIL:Favorite Payees section is  present in the Payee drawer");
+		else
+			Commentary.log(sa, LogStatus.PASS, "PASS: Favorite Payees section is not present in the Payee drawer");
+    
+    	Thread.sleep(5000);
+		sa.assertAll();
+		
+	}
+	
+	
+	@Test (priority = 6, enabled = true)
+	public void FP6_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+	
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the user has option to disable the 'Display Favorite Payees' settings from the payee drawer itself.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+	
+		fp.EnablefavoritPayee();
+		fp.HideFavoritePayeeFromPayeeDrawer();
+
+		
+        if(Verify.objExists(fp.FavoritePayeeLabel))
+    		
+    		Commentary.log(sa,LogStatus.FAIL, "FAIL:Favorite Payees section is  present in the Payee drawer");
+		else
+			Commentary.log(sa, LogStatus.PASS, "PASS: Favorite Payees section is not present in the Payee drawer");
+    
+    	Thread.sleep(1000);
+		
+		
+		sa.assertAll();
+		
+	}
+	
+	@Test (priority = 7, enabled = true)
+	public void FP7_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+	
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the user is able to add a payee as Favorite and the same is seen under the Favorite Payees section.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+		fp.EnablefavoritPayee();
+
+ 		TransactionDetailPage td = new TransactionDetailPage();
+        
+    	op.addTransaction.click();
+        td.enterAmount("10.00");
+    	Thread.sleep(1000);
+    	fp.payee.click();
+      
+    	Thread.sleep(1000);
+		
+		
+		fp.AddPayeeAsFavPayee("Axis");
+		fp.verifyFavPayee("Axis");
+	
+	
+		Thread.sleep(1000);
+		sa.assertAll();
+		
+	}
+	
+	@Test (priority = 8, enabled = true)
+	public void FP9_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+	
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the search results on the payee drawer should show the Favorite Payees section as well if the searched keyword matches with the Yelp and All payees..");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 6);
+
+ 		TransactionDetailPage td = new TransactionDetailPage();
+        
+    	op.addTransaction.click();
+        td.enterAmount("10.00");
+    	Thread.sleep(1000);
+
+    	fp.payee.click();
+        Thread.sleep(1000);
+	    td.searchPayee.sendKeys("Axis");
+		h.hideKeyBoard();
+		Thread.sleep(3000);
+	   
+	   if(Verify.objExists(fp.FavoritePayeeLabel)) {
+		
+			
+			Commentary.log(LogStatus.PASS,"PASS: Showing favorite payee section on the payee drawer. ");
+		}
+		else {
+
+			Commentary.log(LogStatus.FAIL,"FAIL: Not Showing favorite payee section on the payee drawer. ");
+		}
+	   
+		sa.assertAll();
+		
+	}
+
+	
+	@Test (priority = 9, enabled = true)
+	public void FP11_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+	
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that user is able to remove a payee from the Favorites list.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 6);
+
+ 		TransactionDetailPage td = new TransactionDetailPage();
+ 		fp.EnablefavoritPayee();
+        
+    	op.addTransaction.click();
+        td.enterAmount("10.00");
+    	Thread.sleep(1000);
+        fp.payee.click();
+      
+    	Thread.sleep(1000);
+		
+		
+		fp.RemoveFavPayee("Axis");
+		fp.verifyRemoveFavPayee("Axis");
+	
+		Thread.sleep(1000);
+		sa.assertAll();
+		
+	}
+	
+	@Test (priority = 10, enabled = true)
+	public void FP12_Test() throws Exception {
+		SoftAssert sa = new SoftAssert();
+		Helper h = new Helper();
+	
+		
+		Commentary.log(LogStatus.INFO, "["+h.getEngine()+"]: Verify that the user should not see Favorite Payees section on the Add Reminder series Payee drawer.");
+		
+        FavoritePayeePage fp = new FavoritePayeePage();
+		OverviewPage op = new  OverviewPage();
+		Verify.waitForObjectToDisappear(op.refreshSpinnerIcon, 4);
+		BillsAndIncomePage bi = new BillsAndIncomePage();
+		fp.DisablefavoritPayee();
+		
+		op.tapOnBillsAndIncomeCard();
+		bi.addNewReminderSeries("Bill");
+		bi.payeeName.click();
+		
+	   if(Verify.objExists(fp.FavoritePayeeLabel))
+    		
+    		Commentary.log(sa,LogStatus.FAIL, "FAIL:Favorite Payees section is  present in the Add Reminder series Payee drawer.");
+		else
+			Commentary.log(sa, LogStatus.PASS, "PASS: Favorite Payees section is not present in on the Add Reminder series Payee drawer.");
+	        
+	        Thread.sleep(1000);
+    
+		sa.assertAll();
+		
+	}
+
+}
